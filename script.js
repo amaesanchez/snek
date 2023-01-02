08fc63d6c9b7fd70c48962213da378b4903f8e1e
Arlaine Sanchez <amaesanchez@gmail.com>
2023-01-01 23:38:23 -0500

    initial commit

--- script.js
+++ script.js
@@ -11,24 +11,69 @@ class Boardgame {
    * which will be the # of elements in a row & # of rows
    * - also has
    */
-  constructor(width=50, height=50) {
+  constructor(width = 30, height = 30) {
     this.width = width;
     this.height = height;
-    this.food = "o"
+    this.food = {char: "o", X: null, Y: null};
+    this.matrix = null;
   }
 
   makeGame() {
-    const rows = Array.from({length: height});
-    const matrix = rows.map(row => Array.from({length: width}));
+    const rows = Array.from({ length: this.height });
+    const matrix = rows.map(row => Array.from({ length: this.width }).fill(0));
+    // .map(i => null)
+    this.matrix = matrix;
+  }
 
-    console.log(matrix);
+  placeFood() {
+    // pick and RETURNrandom row and random index
+    // at that location,
+
+    const randomRow = Math.floor(Math.random() * (this.height + 1));
+    const randomCol = Math.floor(Math.random() * (this.width + 1));
+    this.matrix[randomRow][randomCol] = this.food.char;
+
+    this.food.X = randomCol;
+    this.food.Y = randomRow;
+
+    return [randomRow, randomCol];
+
+  }
+
+  startGame() {
+    console.debug("startGame method");
+    this.makeGame();
+    this.placeFood();
   }
 }
+
 
 
 
 /** create snake instance
  * - create method for lengthening snake when it eats food
  */
+class Snake {
+  constructor(y=game.height - 1, x=0) {
+    this.direction = "ArrowRight";
+    this.body = [{X: x, Y: y}];
+  }
 
-const game = new Boardgame();
+  // method for adding a snake cell when it eats food
+  static addSnakeBody(row, cell) {
+    console.debug("addSnakedebug", this.direction);
+
+    const newBody = new Snake(row, cell);
+    this.body.push(newBody);
+    game.matrix[row][cell] = newBody;
+
+    console.log(game.matrix[row])
+  }
+
+  // method for changing the direction of the FRONT of the snake??
+
+
+}
+
+// you dont need an object instance in every cell, just instances for the snake
+// to keep track?
---
+++ ui.js
@@ -0,0 +1,138 @@
+"use strict";
+
+const $GAME_BOARD = $("#gameboard");
+const TIMER_MS = 250;
+const KEYS = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
+let intervalID;
+
+function displayGame() {
+  // create a grid using matrix in memory
+  // append to #gameboard
+  let $tbody = $("<tbody>");
+
+  for (let i = 0; i < game.height; i++) {
+    let $matrixRow = $("<tr>");
+    for (let j = 0; j < game.width; j++) {
+      $matrixRow.append($("<td>"));
+    }
+    $tbody.append($matrixRow);
+  }
+
+  $GAME_BOARD.attr("style", `width:${10 * game.width}px`);
+  $GAME_BOARD.append($tbody);
+}
+
+/** places food item at random cell on grid */
+function displayFood() {
+  let [randomRow, randomCol] = [game.food.Y, game.food.X];
+
+  // get to random cell and change its bg color
+  // (pref with jQuery but couldnt figure it out)
+  // store the food coordinates so when snake reaches food,
+  // new snakebody is added at the food coord?
+
+  const rows = document.querySelectorAll("tr");
+  const foodCell = rows[randomRow].children[randomCol];
+  foodCell.setAttribute("style", "background-color:red");
+}
+
+function displaySnake() {
+  console.debug("displaySnake", snake.body);
+  // make snake instance and place
+  // at the last row, first cell in memory FIRSt
+
+  const rows = document.querySelectorAll("tr");
+  for (let coord of snake.body) {
+    console.log(rows, coord.X, coord.Y);
+    const snake_body = rows[coord.Y].children[coord.X];
+    snake_body.setAttribute("style", "background-color: yellow");
+  }
+}
+
+function updateDirection(evt) {
+  evt.preventDefault();
+  const key = evt.originalEvent.key;
+
+  if (KEYS.includes(key)) {
+    snake.direction = key;
+  }
+}
+
+function update_tile(coord) {
+  const rows = document.querySelectorAll("tr");
+  const cell = rows[coord.Y].children[coord.X];
+  cell.setAttribute("style", "background-color:black");
+}
+
+function moveSnake() {
+  if (!checkForBounds()) {
+    clearTimeout(intervalID);
+    alert('you lose')
+    return
+  }
+
+  const last_body = snake.body[snake.body.length - 1];
+  update_tile(last_body)
+
+  if (snake.direction === "ArrowUp") {
+    const new_body = { X: last_body.X, Y: last_body.Y - 1 };
+    snake.body.unshift(new_body);
+    snake.body.pop();
+  } else if (snake.direction === "ArrowDown") {
+    const new_body = { X: last_body.X, Y: last_body.Y + 1 };
+    snake.body.unshift(new_body);
+    snake.body.pop();
+  } else if (snake.direction === "ArrowLeft") {
+    const new_body = { X: last_body.X - 1, Y: last_body.Y };
+    snake.body.unshift(new_body);
+    snake.body.pop();
+  } else if (snake.direction === "ArrowRight") {
+    const new_body = { X: last_body.X + 1, Y: last_body.Y };
+    snake.body.unshift(new_body);
+    snake.body.pop();
+  }
+  console.log("movesnake", snake.body)
+  displaySnake()
+
+  // checkForFood()
+}
+
+function checkForBounds() {
+  const head = snake.body[0]
+
+  if (head.Y >= game.height || head.Y < 0 || head.X >= game.width || head.X < 0) {
+    return false
+  }
+  return true;
+}
+
+/** Start game */
+function startGame() {
+  $GAME_BOARD.empty();
+
+  game.startGame();
+
+  displayGame();
+  displayFood();
+  displaySnake();
+
+  intervalID = setInterval(moveSnake, TIMER_MS)
+}
+
+const game = new Boardgame();
+const snake = new Snake();
+$("#start").on("click", startGame);
+$(document).keydown(updateDirection);
+
+/** while the snake hasnt hit a wall or itself
+ *
+ * - EVERY 500ms or so, pop the last snake cell and
+ * - push to the front of the cell
+ * - use the snake.direction of the front snake cell to figure out
+ * - where to push the new snake instance
+ * - check for food at each move
+ *
+ * - when snake encounters food "o", add to snake body
+ * - and displayfood again
+ *
+ */
